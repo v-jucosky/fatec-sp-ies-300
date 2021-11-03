0ea6b883591fd251c7e1286a6c270c93e41c157c
Victor Jucosky <vjucosky@gmail.com>
2021-11-02 21:38:11 -0300

    Versão inicial

---
+++ src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { updateDoc, doc, getDoc } from '@firebase/firestore';
+import { Container, Typography, Button, TextField } from '@material-ui/core';
+
+import { database } from '../../utils/settings/firebase';
+
+function ProfilePage({ auth, profile, setProfile, pageHistory }) {
+    const [formData, setFormData] = useState({
+        displayName: profile.data().displayName
+    });
+
+    function submitForm(event) {
+        event.preventDefault();
+
+        updateDoc(doc(database, 'profiles', auth.currentUser.uid), {
+            displayName: formData.displayName
+        }).then(() => {
+            getDoc(doc(database, 'profiles', auth.currentUser.uid))
+                .then(document => {
+                    setProfile(document);
+                    pageHistory.push('/');
+                });
+        });
+    };
+
+    return (
+        <Container maxWidth='md' style={{ marginTop: 64 }}>
+            <Typography gutterBottom variant='h4'>
+                Perfil
+            </Typography>
+            <Typography gutterBottom variant='subtitle1' style={{ marginBottom: 16 }}>
+                Atualize seu perfil
+            </Typography>
+            <form noValidate autoComplete='off' style={{ marginBottom: 16 }}>
+                <TextField
+                    required
+                    fullWidth
+                    id='displayName'
+                    label='Apelido'
+                    variant='outlined'
+                    value={formData.displayName}
+                    onChange={(event) => setFormData({ ...formData, displayName: event.target.value })}
+                    style={{ marginBottom: 16 }}
+                />
+                <Button variant='contained' color='primary' onClick={(event) => submitForm(event)}>
+                    Salvar
+                </Button>
+            </form>
+        </Container>
+    );
+};
+
+export default ProfilePage;
---
+++ src/utils/shared/game.js
@@ -0,0 +1,35 @@
+const TILE_MAXIMUM_NUMBER = 3;
+const DECK_MAXIMUM_SIZE = 3;
+const PLAYER_LIMIT = 2;
+
+function buildSleep() {
+    let sleep = [];
+
+    for (let i = 0; i <= TILE_MAXIMUM_NUMBER; i++) {
+        for (let j = i; j <= TILE_MAXIMUM_NUMBER; j++) {
+            let x = i.toString();
+            let y = j.toString();
+
+            sleep.push({
+                id: x + '_' + y,
+                leftString: x,
+                leftNumber: i,
+                rightString: y,
+                rightNumber: j
+            });
+        };
+    };
+
+    return sleep;
+};
+
+function getTileFromSleep(sleep) {
+    let index = Math.floor(Math.random() * sleep.length);
+    let tile = sleep[index];
+
+    sleep.splice(index, 1);
+
+    return tile;
+};
+
+export { buildSleep, getTileFromSleep, DECK_MAXIMUM_SIZE, PLAYER_LIMIT };
