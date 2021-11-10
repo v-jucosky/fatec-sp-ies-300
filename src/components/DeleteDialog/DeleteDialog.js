07b98a4ac6f2cdf39c910ad35f05cb2d5202c30b
Victor Jucosky <vjucosky@gmail.com>
2021-11-10 20:06:59 -0300

    Versão inicial

---
+++ src/components/DeleteDialog/DeleteDialog.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Typography, Button } from '@material-ui/core';
+
+function DeleteDialog({ dialogContent, setDialogContent }) {
+    function closeDialog() {
+        setDialogContent({ ...dialogContent, open: false });
+    };
+
+    function deleteItem() {
+        dialogContent.onDelete();
+        closeDialog();
+    };
+
+    return (
+        <Dialog maxWidth='sm' fullWidth={true} open={dialogContent.open} onClose={() => closeDialog()}>
+            <DialogTitle>
+                Excluir
+            </DialogTitle>
+            <DialogContent>
+                <Typography gutterBottom>
+                    Tem certeza de que deseja excluir “{dialogContent.name}”? Não será possível desfazer esta operação.
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button color='primary' onClick={() => closeDialog()}>
+                    Cancelar
+                </Button>
+                <Button color='primary' onClick={() => deleteItem()}>
+                    Excluir
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DeleteDialog;
