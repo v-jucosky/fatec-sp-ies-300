91bbaea12d0000189c0332eb12a8c0c8a188de58
Victor Jucosky <vjucosky@gmail.com>
2021-11-12 19:48:01 -0300

    Configuração das variáveis de ambiente

--- src/utils/settings/firebase.js
+++ src/utils/settings/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_APP_ID,
+    projectId: process.env.REACT_APP_AUTH_DOMAIN,
+    storageBucket: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.REACT_APP_PROJECT_ID,
+    appId: process.env.REACT_APP_STORAGE_BUCKET
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
