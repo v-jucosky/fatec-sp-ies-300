6650b76bab9a9910b68be891b2e2418a90a02bab
Victor Jucosky <vjucosky@gmail.com>
2021-11-14 14:00:59 -0300

    Correção nas variáveis de ambiente

--- src/utils/settings/firebase.js
+++ src/utils/settings/firebase.js
@@ -3,11 +3,11 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_APP_ID,
-    projectId: process.env.REACT_APP_AUTH_DOMAIN,
-    storageBucket: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    messagingSenderId: process.env.REACT_APP_PROJECT_ID,
-    appId: process.env.REACT_APP_STORAGE_BUCKET
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
