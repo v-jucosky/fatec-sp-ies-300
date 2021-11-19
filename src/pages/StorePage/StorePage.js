68ab0359610b7ff0d92fa3af094e95a3708b67db
Victor Jucosky <vjucosky@gmail.com>
2021-11-19 20:50:10 -0300

    Melhorias na UI/UX

--- src/pages/StorePage/StorePage.js
+++ src/pages/StorePage/StorePage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Typography, Card, CardContent, CardActions, Grid, Button } from '@material-ui/core';
+import { Container, Typography, Card, CardContent, CardActions, Grid, Button, CardActionArea } from '@material-ui/core';
 
 import PurchaseDialog, { purchaseDialogDefaultContent } from '../../components/PurchaseDialog';
 import { FEATURE, THEME } from '../../utils/utils/type';
@@ -24,23 +24,25 @@ function StorePage({ auth, profile, themes, purchases }) {
                         return (
                             <Grid item xs={6} md={3} style={{ display: 'flex' }}>
                                 <Card style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', width: '100%' }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant='h6'>
-                                            {theme.name}
-                                        </Typography>
-                                        <Typography gutterBottom>
-                                            {theme.description}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button color='primary' size='small' disabled={purchases.map(purchase => purchase.item.id).includes(theme.id)} onClick={() => setPurchaseDialogContent({ ...purchaseDialogContent, type: THEME, item: { ...theme }, isOpen: true })}>
-                                            {theme.price > 0 ?
-                                                'R$' + theme.price.toFixed(2)
-                                                :
-                                                'Grátis'
-                                            }
-                                        </Button>
-                                    </CardActions>
+                                    <CardActionArea disabled={purchases.map(purchase => purchase.item.id).includes(theme.id)} onClick={() => setPurchaseDialogContent({ ...purchaseDialogContent, type: THEME, item: { ...theme }, isOpen: true })}>
+                                        <CardContent>
+                                            <Typography gutterBottom variant='h6'>
+                                                {theme.name}
+                                            </Typography>
+                                            <Typography gutterBottom>
+                                                {theme.description}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button color='primary' size='small' disabled={purchases.map(purchase => purchase.item.id).includes(theme.id)}>
+                                                {theme.price > 0 ?
+                                                    'R$' + theme.price.toFixed(2)
+                                                    :
+                                                    'Grátis'
+                                                }
+                                            </Button>
+                                        </CardActions>
+                                    </CardActionArea>
                                 </Card>
                             </Grid>
                         );
@@ -65,6 +67,23 @@ function StorePage({ auth, profile, themes, purchases }) {
                             </CardContent>
                             <CardActions>
                                 <Button disabled color='primary' size='small' onClick={() => setPurchaseDialogContent({ ...purchaseDialogContent, type: FEATURE, item: { name: 'Destaque de jogadas possíveis', price: 29.99 }, isOpen: true })}>
+                                    Em breve
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} md={6} style={{ display: 'flex' }}>
+                        <Card style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', width: '100%' }}>
+                            <CardContent>
+                                <Typography gutterBottom variant='h6'>
+                                    Envio de mensagens nos jogos
+                                </Typography>
+                                <Typography gutterBottom>
+                                    Você poderá enviar mensagens para todos os jogadores de um jogo. As mensagens aparecerão na cor do seu tema!
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button disabled color='primary' size='small' onClick={() => setPurchaseDialogContent({ ...purchaseDialogContent, type: FEATURE, item: { name: 'Envio de mensagens nos jogos', price: 29.99 }, isOpen: true })}>
                                     Em breve
                                 </Button>
                             </CardActions>
